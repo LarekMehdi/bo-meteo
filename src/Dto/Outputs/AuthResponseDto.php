9b2a5eb53e2beafd8e0e1bb43f48a123349de213
mehdi-larek <larek.med@gmail.com>
2026-02-03 18:32:58 +0100

    test: ajouts des tests de AuthService

--- src/Dto/Outputs/AuthResponseDto.php
+++ src/Dto/Outputs/AuthResponseDto.php
@@ -10,4 +10,40 @@ class AuthResponseDto
         public readonly UserDto $user,
     ) {
     }
+
+    public function getAccessToken(): string
+    {
+        return $this->accessToken;
+    }
+
+    public function setAccessToken(string $accessToken): static
+    {
+        $this->accessToken = $accessToken;
+
+        return $this;
+    }
+
+    public function getRefreshToken(): ?string
+    {
+        return $this->refreshToken;
+    }
+
+    public function setRefreshToken(?string $refreshToken): static
+    {
+        $this->refreshToken = $refreshToken;
+
+        return $this;
+    }
+
+    public function getUser(): UserDto
+    {
+        return $this->user;
+    }
+
+    public function setUser(UserDto $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
 }
